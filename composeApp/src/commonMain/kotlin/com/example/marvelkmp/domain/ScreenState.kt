b533e865e8343a00042a129d717c542aa90c8aa2
Gonzalo <gnizzi1990@gmail.com>
2026-06-19 12:07:26 -0300

    agregar estado a ScreenState.kt

--- composeApp/src/commonMain/kotlin/com/example/marvelkmp/domain/ScreenState.kt
+++ composeApp/src/commonMain/kotlin/com/example/marvelkmp/domain/ScreenState.kt
@@ -3,4 +3,10 @@ package com.example.marvelkmp.domain
 sealed interface ScreenState {
     data object Loading : ScreenState
     data class ShowCharacters(val characters: List<Character>) : ScreenState
+
+    data class ShowDetail(val character: Character) : ScreenState
+
+
+
+
 }
