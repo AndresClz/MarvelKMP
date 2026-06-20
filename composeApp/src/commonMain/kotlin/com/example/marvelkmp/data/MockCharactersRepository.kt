7bb71cdadb837dd6daa89e1cf990a9c3595c46e8
Andres Calizaya <42985981+AndresClz@users.noreply.github.com>
2026-06-20 16:11:39 -0300

    Fixes characters thumbnail URLs (#12)

--- composeApp/src/commonMain/kotlin/com/example/marvelkmp/data/MockCharactersRepository.kt
+++ composeApp/src/commonMain/kotlin/com/example/marvelkmp/data/MockCharactersRepository.kt
@@ -18,37 +18,37 @@ class MockCharactersRepository : CharactersRepository {
                 id = 1009368,
                 name = "Iron Fist",
                 description = "Trained in the martial arts of K'un-Lun, Danny Rand returned to New York and became the Immortal Iron Fist.",
-                thumbnailUrl = "http://i.annihil.us/u/prod/marvel/i/mg/3/f0/51424257af2f4/standard_fantastic.jpg",
+                thumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/SDCC_2012_-_Iron_Fist_%287574609830%29.jpg/960px-SDCC_2012_-_Iron_Fist_%287574609830%29.jpg",
             ),
             Character(
                 id = 1009664,
                 name = "Thor",
                 description = "As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir.",
-                thumbnailUrl = "http://i.annihil.us/u/prod/marvel/i/mg/7/10/537bc71e9286f/standard_fantastic.jpg",
+                thumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Cosplay_of_Thor_%28Marvel%29_at_Made_in_Asia_2022_%2852104540946%29.jpg/960px-Cosplay_of_Thor_%28Marvel%29_at_Made_in_Asia_2022_%2852104540946%29.jpg",
             ),
             Character(
                 id = 1017100,
                 name = "Iron Man",
                 description = "Genius. Billionaire. Playboy. Philanthropist. Tony Stark's got it all, and that's just the beginning.",
-                thumbnailUrl = "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55/standard_fantastic.jpg",
+                thumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Cosplay_of_Iron_Man_at_GEMA_2022.jpg/960px-Cosplay_of_Iron_Man_at_GEMA_2022.jpg",
             ),
             Character(
                 id = 1011490,
                 name = "Rocket Raccoon",
                 description = "",
-                thumbnailUrl = "http://i.annihil.us/u/prod/marvel/i/mg/9/b0/50fec1e49298a/standard_fantastic.jpg",
+                thumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/C2E2_2015_-_Rocket_Raccoon_%2816683662034%29.jpg/960px-C2E2_2015_-_Rocket_Raccoon_%2816683662034%29.jpg",
             ),
             Character(
                 id = 1010903,
                 name = "Groot",
                 description = "",
-                thumbnailUrl = "http://i.annihil.us/u/prod/marvel/i/mg/3/10/526033c8b474a/standard_fantastic.jpg",
+                thumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/C2E2_2016_Contest_-_Rocket_Racoon_%26_Groot_%2834129673716%29.jpg/960px-C2E2_2016_Contest_-_Rocket_Racoon_%26_Groot_%2834129673716%29.jpg",
             ),
             Character(
                 id = 1009562,
                 name = "Scarlet Witch",
                 description = "",
-                thumbnailUrl = "http://i.annihil.us/u/prod/marvel/i/mg/6/70/5261a7d7e607c/standard_fantastic.jpg",
+                thumbnailUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/2021-10-30_11-00-32_ILCE-7C_DSC06467_DxO.jpg/960px-2021-10-30_11-00-32_ILCE-7C_DSC06467_DxO.jpg",
             ),
         )
     }
